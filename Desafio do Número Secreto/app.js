f1f953fbcc3fcdb99597b46d463c9b8771e96a5f
Aiara Dnls <aiara.dnls@gmail.com>
2025-07-31 21:58:28 -0300

    Implementa funcionalidades adicionais

--- Desafio do Número Secreto/app.js
+++ Desafio do Número Secreto/app.js
@@ -1,22 +1,36 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = Math.floor(Math.random() * 100) + 1;
-let tentativa = 1;
-console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 100');
+let jogarNovamente = "S";
 
-//enquanto chute não for igual ao n.s.
-while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
-    // se chute for igual ao número secreto
-    if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto} com ${tentativa} tentativas.`);
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
+while (jogarNovamente.toUpperCase() == "S") {
+    alert('Boas vindas ao jogo do número secreto');
+
+    let modo = prompt('Escolha o modo do jogo: 1 para Normal, 2 para Difícil');
+    let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    let tentativas = 1;
+    let limiteTentativas = (modo == "2") ? 5 : Infinity;
+    console.log(numeroSecreto);
+    let chute;
+
+    //enquanto chute não for igual ao n.s.
+    while (chute != numeroSecreto) {
+        chute = prompt('Escolha um número entre 1 e 100');
+        // se chute for igual ao número secreto
+
+        if (chute == numeroSecreto) {
+            alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativas.`);
         } else {
-            alert(`O número secreto é maior que ${chute}`);
+            if (chute > numeroSecreto) {
+                alert(`O número secreto é menor que ${chute}`);
+            } else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
+            //Soma das tentativas do usuário.
+            tentativas ++;
+
+            if(tentativas > limiteTentativas) {
+                alert(`Suas ${limiteTentativas} tentativas acabaram! O número secreto era ${numeroSecreto}.`);
+                break;
+            }
         }
-        //Soma das tentativas do usuário.
-        tentativa ++;
     }
-}
+    jogarNovamente = prompt('Deseja jogar novamente? S para sim ou N para não');
+} 
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Desafio do Número Secreto
-Desenvolvido em aula de JS sobre While, e aprimorado para o número secreto ser sempre um número aleatório.
+Desenvolvido em aula:
+Desenvolvido a estrutura do jogo com dicas em aula de JS sobre While com a Alura;
+
+Desenvolvido fora da aula: 
+Aprimorado para o número secreto ser sempre um número aleatório;
+Adicionado o modo de jogo Dificil, para o usuário ter de adivinhar em até 5 tentativas;
+E colocado o jogo para rodar em looping enquanto o usuário desejar continuar.
